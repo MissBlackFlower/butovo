494af18d0158fc1268d1fb2cef4ed24b6e7d1e9d
Viktoriia <babenko_v_v@ukr.net>
2016-05-31 18:13:41 +0300

    slick

--- build/js/common.js
+++ build/js/common.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
-
-	// go js
-
+	$('.apartments__conteiner').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  adaptiveHeight: true,
+	prevArrow: '.slick-left',
+  nextArrow: '.slick-right',
+	responsive: [
+    {
+      breakpoint: 1220,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 840,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
+	});
 });
