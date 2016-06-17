6a9802aa331e862039a3f355af3db54ac3f348b1
Viktoriia <babenko_v_v@ukr.net>
2016-06-17 17:47:37 +0300

    upd

--- build/js/common.js
+++ build/js/common.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	// заполняем лейблы номерами квартир для выбранного этажа
 	$('.storey__section input').on('change', function(){
 		var floor = $(this).attr('value')
 		for (var n = 0; n <= 9; n++) {
@@ -12,6 +13,7 @@ $(document).ready(function() {
 	$('.apartments__conteiner').slick({
   dots: false,
   infinite: true,
+	// autoplay: true,
   speed: 300,
   slidesToShow: 3,
   adaptiveHeight: true,
@@ -29,7 +31,40 @@ $(document).ready(function() {
       settings: {
         slidesToShow: 1,
       }
-    }
+    },
   ]
 	});
+// показывать кнопку меню при ширине окна меньше 1000
+	$('body').ready(function(){
+		if ($('body').width() <= 1150) {
+			$('.js-header').removeClass('is-active');
+			$('.js-header-nav').slideUp(200);
+		}
+	});
+// открыть навигацию при клике на кнопку меню
+	$('.js-btn-menu').on('click', function(event){
+		var this_ = $(this),
+			parent = this_.parents('.js-header'),
+			nav = parent.find('.js-header-nav');
+		if (!parent.hasClass('is-active')){
+			parent.addClass('is-active');
+			nav.slideDown(200);
+		}
+		else {
+			parent.removeClass('is-active');
+			nav.slideUp(200);
+		}
+		event.stopPropagation();
+	});
+
+	$h = $('.js-header').offset().top;
+	$(window).scroll(function(){
+        // Если прокрутили скролл ниже макушки блока, включаем фиксацию
+		if ( ($(window).scrollTop() > $h) && ($('body').width() <= 1150) ) {
+	        	$(".js-header").css({"padding": "10px" , "position":"fixed", "top":0 , "left":0 , "z-index":50 , "background-color": "#fff" , "border-bottom":"solid 1px #61c1f3"});
+	        }else{
+	            //Иначе возвращаем всё назад. Тут вы вносите свои данные
+	        $(".js-header").css({"position":"static", "padding": "0", "border-bottom":"none"});
+	        }
+	  });
 });
